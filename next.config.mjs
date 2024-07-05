2192f548c9a16b64ed03e0a76c8f87d53cc6a192
JesusOfLagos <jesuswrites20043@gmail.com>
2024-07-05 19:34:13 +0100

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+
+
+
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    // Add any other custom configurations here
+  };
+  
+  export default nextConfig;
+  
