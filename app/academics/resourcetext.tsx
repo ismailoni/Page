d77071658f816c3170336c1ec8ff9be8ee9169ef
Oni Ismail Modupe <93334212+ismailoni@users.noreply.github.com>
2025-07-14 17:11:12 +0100

    Delete app/academics directory

--- app/academics/resourcetext.tsx
+++
@@ -1,9 +0,0 @@
-export const ResourceText = () => {
-    return (
-        <div className="flex flex-col justify-center gap-4">
-            <p style={{ color: '#49C4A2' }} className="text-center font-bold text-2xl">All Resources</p>
-            <h1 className="font-semibold text-3xl text-center">The most resourcesful material source</h1>
-            <p className="text-gray-500 text-center text-sm">Explore our comprehensive range of programs, from regular and weekly sessions to special adhoc events,<br /> designed to enrich your journey with MSSN Unilag.</p>
-        </div>
-    )
-}
